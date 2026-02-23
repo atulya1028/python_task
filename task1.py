6270a7bb10bf11e052938a979af721804e0ebd2b
atulya1028 <atulyaagrawal1996@gmail.com>
2026-02-23 18:14:06 +0530

    My Task

---
+++ task1.py
@@ -0,0 +1,12 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+add = int(num1) + int(num2)
+sub = int(num1) - int(num2)
+mul = int(num1) * int(num2)
+div = int(num1) / int(num2)
+
+print("Addition:",add)
+print("Subtraction:",sub)
+print("Multiplication:",mul)
+print("Division:",div)
